d2b87618ab020b965d6512da04661e7f4334e490
Ikade Monday <shadrachgeek@gmail.com>
2022-07-14 15:23:41 +0100

    /* Challenge_2; returning all even numbers from the given array "digits"  by using the filter method, passsing in a function and verifying  there is no remainder on each iteration by dividing each number by 2*/

--- src/assignment.js
+++ src/assignment.js
@@ -38,30 +38,12 @@ const numbers = [
     -91,
     -50
 ];
-function sumOfNumbers(
-    68,
-    -68,
-    27,
-    94,
-    72,
-    -25,
-    -51,
-    32,
-    10,
-    64,
-    -94,
-    4,
-    34,
-    -86,
-    90,
-    81,
-    20,
-    -56,
-    -91,
-    -50) {
-    return numbers.length;
-    console.log (sumOfNumbers)
-}
+       /*Challenge_1; using the "reduce()" method to find and calculate
+       the sum of the given array of numbers*/
+var sum = numbers.reduce(function(a, b){
+    return a + b;
+}, 0);
+console.log (sum);
 /**
  * Challenge - 2
  * 
@@ -71,11 +53,41 @@ function sumOfNumbers(
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
-function countEvenNumbers(arrayOfNumbers) {
+ function countEvenNumbers(arrayOfNumbers) {
     return arrayOfNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
 
+
+    const digits = [
+        17,
+        0,
+        67,
+        41,
+        49,
+        21,
+        1,
+        86,
+        76,
+        2,
+        54,
+        14,
+        93,
+        84,
+        14,
+        77,
+        35,
+        7,
+        29,
+        64
+    ]
+    /* Challenge_2; returning all even numbers from the given array "digits" 
+by using the filter method, passsing in a function and verifying 
+there is no remainder on each iteration by dividing each number
+by 2*/
+    const evenNumbers = digits.filter(number =>{
+        return number % 2 === 0;
+    })
+    console.log(evenNumbers)
 /**
  * Challenge - 3
  * 
@@ -85,7 +97,7 @@ function countEvenNumbers(arrayOfNumbers) {
  * E.g 51.21 should just be 51 (hint: Math.trunc(...) function)
  * If you did Challenge - 3, remove the comment in the line just after this function
  * 
- * See https://www.thoughtco.com/celcius-to-farenheit-formula-609227 for the conversion formula
+ * See https://www.thoughtco.com/celcius-to-farenheit-formula-609(227 for the conversion formula
  * 
  * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
  * @returns Array the converted temperatures in Fahrenheit
